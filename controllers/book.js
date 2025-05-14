97d7ed13c68684760b8329f41c75783a48d6c8e1
ayanadiya <ayanadiya17@gmail.com>
2025-05-14 09:38:39 +0530

    Added functionalities for book

--- controllers/book.js
+++ controllers/book.js
@@ -8,9 +8,79 @@ exports.postBook= async (req,res)=>{
             res.status(400).json({message:"All fields are required"});
         }
         const book= await Book.create({
-            
-        })
+           title:title,
+           author:author,
+           isbn:isbn,
+           price:price,
+           stock:stock 
+        });
+        res.status(201).json({book});
     } catch (error) {
-        
+        console.log(error);
+        res.status(500).json({message:"Something went wrong"});
+    }
+}
+
+exports.getBooks= async (req, res)=>{
+    try {
+        const books= await Book.findAll();
+        res.status(200).json({books})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({messsage:"Something went wrong"});
+    }
+}
+
+exports.getBookById= async (req, res)=>{
+    try {
+        const id=req.params.id;
+        const book= await Book.findAll({where:{id:id}});
+        res.status(200).json({book});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Something went wrong"});
+    }
+}
+
+exports.editbook=async (req,res)=>{
+    try{
+        const {newTitle, newAuthor, newIsbn, newPrice, newStock}=req.body
+        const id=req.params.id;
+        const book=await Book.findByPk(id)
+        book.title=newTitle;
+        book.author=newAuthor;
+        book.isbn=newIsbn;
+        book.price=newPrice;
+        book.stock=newStock;
+        await book.save();
+        res.status(200).json({book});
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message:"Something went wrong"});
+    }
+}
+
+exports.updateStock= async(req,res)=>{
+    try {
+        const id=req.params.id;
+        const book= await Book.findByPk(id);
+        book.stock=req.body.newStock
+        await book.save();
+        res.status(200).json({meassage:"Stock updated successfully"});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Something went wrong"});
+    }
+}
+
+exports.deleteBook= async(req,res)=>{
+    try {
+        const id=req.params.id;
+        const book= await Book.findByPk(id);
+        await book.destroy();
+        res.status(200).json({message:"Book removed successfully"});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Something went wrong"});
     }
 }
